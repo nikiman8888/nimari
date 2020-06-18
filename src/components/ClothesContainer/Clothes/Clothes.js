0eaddfe52c14824191dfeff4f055afe7e4f92218
nikiman8888 <nikiman88@yahoo.com>
2020-06-18 17:46:00 +0300

    Chanche contacts css

--- src/components/ClothesContainer/Clothes/Clothes.js
+++ src/components/ClothesContainer/Clothes/Clothes.js
@@ -3,7 +3,7 @@ import "./Clothes.css";
 import img from  '../../../images/bluza1.png'
 const Clothes = (props) => {
     const name = props.name;
-    const path = `/images/clothes/${name}.png`;
+    //const path = `/images/clothes/${name}.png`;
    
   return (
     <div className="clothes-single">
