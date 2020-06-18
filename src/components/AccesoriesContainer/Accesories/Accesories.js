0416cbf9d28858deb54ae5617533ad8b1af04bf2
nikiman8888 <nikiman88@yahoo.com>
2020-06-18 17:30:27 +0300

    test

--- src/components/AccesoriesContainer/Accesories/Accesories.js
+++ src/components/AccesoriesContainer/Accesories/Accesories.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Accesories.css';
+import img from '../../../images/acc1.png'
 
 const Accesories = (props) => {
     const name = props.name;
-    const path = `/images/accesories/${name}.png`;
-   
+    //const path = `../../../images/accesories/${name}.png`;
+   //const path = '../../../images/clothes1.jpg';
   return (
     <div className="accesories-single">
-      <img src = {path} />
-      <div>{name}</div>
+      <img src = {img} />
+  <div>{'accesories'}</div>
       
     </div>
   );
--- src/components/ClothesContainer/Clothes/Clothes.js
+++ src/components/ClothesContainer/Clothes/Clothes.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Clothes.css";
-
+import img from  '../../../images/bluza1.png'
 const Clothes = (props) => {
     const name = props.name;
     const path = `/images/clothes/${name}.png`;
    
   return (
     <div className="clothes-single">
-      <img src = {path} />
-      <div>{name}</div>
+      <img src = {img} />
+      <div>{'Clothes'}</div>
       
     </div>
   );
