a4212369c79bf8683d3eada9043869e33f7dac6c
nikiman8888 <nikiman88@yahoo.com>
2020-07-12 00:19:49 +0300

    Updates

---
+++ precache-manifest.2ecacc6cba00c057d3c346726c6b5724.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e14f0c6685fbda50d0c0e2ff4ae413",
+    "url": "/nimari/index.html"
+  },
+  {
+    "revision": "e3038bb25ece11d4f434",
+    "url": "/nimari/static/css/2.98eef212.chunk.css"
+  },
+  {
+    "revision": "dead120edd67214f8f46",
+    "url": "/nimari/static/css/main.83a4c7c8.chunk.css"
+  },
+  {
+    "revision": "e3038bb25ece11d4f434",
+    "url": "/nimari/static/js/2.d6ee315f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nimari/static/js/2.d6ee315f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dead120edd67214f8f46",
+    "url": "/nimari/static/js/main.adad117c.chunk.js"
+  },
+  {
+    "revision": "edaf5b00ff50b525fb64",
+    "url": "/nimari/static/js/runtime-main.c4fe5215.js"
+  },
+  {
+    "revision": "344a129f1ac834b5c1d09361f7d7028a",
+    "url": "/nimari/static/media/acc1.344a129f.png"
+  },
+  {
+    "revision": "481b7b222236d58a1becdc6e80492695",
+    "url": "/nimari/static/media/bluza1.481b7b22.png"
+  },
+  {
+    "revision": "859e6d741391319602b6b4992bd30af3",
+    "url": "/nimari/static/media/flower1.859e6d74.jpg"
+  },
+  {
+    "revision": "11c6568975ee1262b2402a3c02b10d1a",
+    "url": "/nimari/static/media/hom2.11c65689.jpg"
+  },
+  {
+    "revision": "1a80df133d0a777a1b23b70c3c78735e",
+    "url": "/nimari/static/media/services1.1a80df13.png"
+  },
+  {
+    "revision": "74b41902226e1840096ca9dd50158e6d",
+    "url": "/nimari/static/media/slider1.74b41902.jpg"
+  }
+]);
